0d04a3bef516b35c82896ab1c51f6b5a11bf7568
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-04-28 20:08:24 +0300

    get mock list of applications on input change

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,30 @@
 import {useState} from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
+import {FindApplications} from "../wailsjs/go/main/App";
 
 function App() {
     const [input, setInput] = useState('');
+    const [applications, setApplications] = useState<string[]>([]);
+    const [listItems, setListItems] = useState<JSX.Element[]>([]);
     const updateInput = (e: any) => setInput(e.target.value);
+
+    const search = (e: any) => {
+        const input = e.target.value;
+        setInput(input);
+        FindApplications(input).then((result) => {
+            console.log(result);
+            setApplications(result);
+            const items = result.map(i => <li>{i}</li>);
+            setListItems(items);
+        });
+    }
 
     return (
         <div id="App">
             <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateInput} autoComplete="off" name="input" type="text"/>
+                <input id="name" className="input" onChange={search} autoComplete="off" name="input" type="text"/>
+                <ul>{listItems}</ul>
             </div>
         </div>
     )
