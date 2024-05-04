d8c7d4679ff24d57ffc2bd48909b336523dae39f
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-05-04 18:43:08 +0300

    split input and results list into separate components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
 import {FindApplications} from "../wailsjs/go/main/App";
+
+
+interface InputBoxProps {
+    value: string;
+    onChange: (e: any) => void;
+}
+
+interface ResultsBoxProps {
+    items: JSX.Element[];
+}
 
 function App() {
     const [input, setInput] = useState('');
     const [applications, setApplications] = useState<string[]>([]);
     const [listItems, setListItems] = useState<JSX.Element[]>([]);
-    const updateInput = (e: any) => setInput(e.target.value);
 
     const search = (e: any) => {
         const input = e.target.value;
@@ -20,11 +29,37 @@ function App() {
         });
     }
 
+    const InputBox = (props: InputBoxProps) => {
+        return (
+            <input
+                id="name"
+                autoFocus
+                className="w-full bg-neutral-900 text-slate-300 p-4 border-0 focus:outline-none"
+                onChange={props.onChange}
+                autoComplete="off"
+                name="input"
+                type="text"
+                value={props.value}
+            />
+        );
+    }
+
+    const ResultsBox = (props: ResultsBoxProps) => {
+        if (props.items.length === 0) {
+            return (<></>);
+        }
+
+        return (
+            <ul></ul>
+        );
+
+    }
+
     return (
-        <div id="App">
-            <div id="input">
-                <input id="name" onChange={search} autoComplete="off" name="input" type="text"/>
-                <ul>{listItems}</ul>
+        <div id="App" className="text-4xl">
+            <div id="input" className="border-4 border-neutral-800">
+                <InputBox value={input} onChange={search}/>
+                <ResultsBox items={listItems}/>
             </div>
         </div>
     )
