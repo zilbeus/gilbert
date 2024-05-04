ad478d1d93db2f70eb1febe7f27698c318c8e1a0
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-05-04 16:41:38 +0300

    fix glitchy app window appearance

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
 )
 
 //go:embed all:frontend/dist
@@ -27,7 +28,10 @@ func main() {
 			Assets: assets,
 		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		Linux: &linux.Options{
+			WindowIsTranslucent: true,
+		},
+		OnStartup: app.startup,
 		Bind: []interface{}{
 			app,
 		},
